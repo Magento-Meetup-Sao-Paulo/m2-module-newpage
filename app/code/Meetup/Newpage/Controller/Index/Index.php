27b6068ab80cc2c076c3b8634fe6af30f2c1688f
Rafael Corrêa Gomes <rafaelcg_stz@hotmail.com>
2017-06-22 00:50:56 -0300

    Init

---
+++ app/code/Meetup/Newpage/Controller/Index/Index.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Meetup\Newpage\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
+{
+    /** @var PageFactory  */
+    protected $resultPageFactory;
+
+    /**
+     * Index constructor.
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+    /**
+     * Load the page defined in view/frontend/layout/newpage_index_index.xml
+     *
+     * @return \Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+        return $this->resultPageFactory->create();
+    }
+}
---
+++ app/code/Meetup/Newpage/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Meetup_Newpage',
+    __DIR__
+);
